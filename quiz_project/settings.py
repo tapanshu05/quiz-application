d10704d511a0416d9b7bfb0e24ecbdf4cb64dac2
Tapanshu Dutt <tapanshudutt@gmail.com>
2026-07-02 23:41:34 +0530

    Configure allowed hosts for production link

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i^kd+uutfyvce12vu+)&(o#xnzq*k&(ft#vy-bhl)s2k1vj3+5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'quiz-application-dbo8.onrender.com']
 
 
 # Application definition
