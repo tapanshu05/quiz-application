a1ff45d9850cbd63a84a9293d9533fdb6c0c5907
Tapanshu Dutt <tapanshudutt@gmail.com>
2026-07-03 15:31:05 +0530

    Fix templates lookup directory path

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -57,10 +57,12 @@ ROOT_URLCONF = 'quiz_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+       
+        'DIRS': [BASE_DIR / 'quiz_app/templates'], 
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
