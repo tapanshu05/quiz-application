70c298ebffcd9f4f5fb579a52bc7012d2a21266c
Tapanshu Dutt <tapanshudutt@gmail.com>
2026-07-07 00:46:40 +0530

    Secure urls.py by removing setup script

--- quiz_project/urls.py
+++ quiz_project/urls.py
@@ -7,10 +7,3 @@ urlpatterns = [
     path('', include('quiz_app.urls')),       # बाकी सारे पेजेस का रास्ता quiz_app संभालेगा
 ]
 
-from django.contrib.auth.models import User
-try:
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-        print("Superuser 'admin' created successfully!")
-except Exception:
-    pass
